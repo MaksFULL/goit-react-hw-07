a642d81b031c2d84aa11c30eaf0b678b01fa8da5
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2025-01-27 15:20:59 +0200

    contact fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contactsSlice";
-import filtersReducer from "./filtersSlice";
+import s from "./Contact.module.css";
+import { FaTrashAlt } from "react-icons/fa";
+import { MdPhone } from "react-icons/md";
+import { BsPersonFill } from "react-icons/bs";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsOps";
 
-export const store = configureStore({
-  reducer: { contacts: contactsReducer, filters: filtersReducer },
-});
+const Contact = ({ contact }) => {
+  const dispatch = useDispatch();
 
-export default store;
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <li className={s.listItem}>
+      <div className={s.itemBox}>
+        <p className={s.name}>
+          <BsPersonFill className={s.icon} />
+          {contact.name}
+        </p>
+        <p className={s.number}>
+          <MdPhone className={s.icon} />
+          {contact.number}
+        </p>
+      </div>
+      <button onClick={() => handleDelete(contact.id)} className={s.btn}>
+        <FaTrashAlt />
+      </button>
+    </li>
+  );
+};
+
+export default Contact;
